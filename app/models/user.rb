2bb82ab1e01a7d748abda63b37d6dd47c9cdb525
Hassan Habib <muhammadhassanhabib1@gmail.com>
2022-09-12 14:27:17 +0500

    add client and project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
+  after_initialize :set_default_role, if: :new_record?
+
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
  # validates :password, presence: true
-  enum role: { manager: 0, developer: 1, qa: 2 }
+  enum role: { manager: 0, developer: 1, admin: 2 }
   has_one_attached :avatar
+
+  private
+   def set_default_role
+    self.role ||= :admin
+   end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :clients
+  resources :projects
   get 'home/index'
   devise_for :users
   root to: "home#index"
